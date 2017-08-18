4a3044854f1c1a6bac9b54525d1576f120fed2b5
Janardhanan <shaiju.janardhanan@siemens.com>
2017-08-18 22:39:59 +0530

    added math library code

---
+++ MathsLibrary/OperationsClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsLibrary
+{
+    public class OperationsClass
+    {
+        public int AddInt(int num1, int num2)
+        {
+            int sum = 0;
+            sum = num1 + num2;
+            return sum;
+        }
+
+        public string DivInt(int num1, int num2)
+        {
+            if(num2 == 0)
+            {
+                return "NA";
+            }
+
+            int result = num1 / num2;
+            return result.ToString();
+        }
+    }
+}
---
+++ MathsLibraryTests/MathUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MathsLibrary;
+
+namespace MathsLibraryTests
+{
+    [TestFixture]
+    public class MathUnitTests
+    {
+        OperationsClass opClass;
+        
+        [SetUp]
+        public void Initialise()
+        {
+            opClass = new OperationsClass();
+        }
+
+        [Test]
+        public void Test_Add_NonZero()
+        {
+            int a = 1, b = 1;
+            Assert.AreEqual(2, opClass.AddInt(a, b));
+        }
+
+        [Test]
+        public void Test_Div_NonZero()
+        {
+            int a = 1, b = 1;
+            Assert.AreEqual("1", opClass.DivInt(a, b));
+        }
+
+        [Test]
+        public void Test_Div_Zero()
+        {
+            int a = 1, b = 0;
+            Assert.AreEqual("NA", opClass.DivInt(a, b));
+        }
+    }
+}
